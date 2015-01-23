1bde2bf83d14c198f833fa02d936c84018f9a3a6
Chris Capasso <coloristchris@gmail.com>
2015-01-23 11:29:50 -0500

    merged css/js

---
+++ public/javascript/script.js
@@ -0,0 +1,13 @@
+function cycleImage(){
+	var $active = $('#cycler.active');
+	var $next = ($active.next().length >0)? $active.next() : $('#cycler img: first');
+	$next.css('z-index', 2);
+	$active.fadeOut(1500, function(){
+		$active.css('z-index' 1).show().removeClass('active');
+		$next.css('z-index', 3).addClass('active');
+	});
+}
+
+$(document).ready(function(){
+	setInterval('cycleImages()', 7000);
+})
